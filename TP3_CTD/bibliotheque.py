0fce57b174ba5ab49f14e6bcb95ba422d0fc612d
Haelia <sarahwissocq@free.fr>
2016-02-03 20:11:20 +0100

    Bibliotheque fini

--- TP3_CTD/bibliotheque.py
+++ TP3_CTD/bibliotheque.py
@@ -4,29 +4,62 @@ PETIT Antoine & WISSOCQ Sarah
 Test Bibliotheque
 """
 
-class ServiceEmprunt():
+class ServiceEmprunt:
 
-	def __init__(self):
-		pass
+    def __init__(self, les_emprunts, les_litiges):
+        self.fabrique_emprunts = les_emprunts
+        self.fabrique_litiges = les_litiges
 
-	def emprunter(self,livre,membre):
-		if not livre.est_empruntable():
-			raise LivreNonEmpruntable()
-		raise QuotaAtteintError()
+    def rendre(self,livre,membre):
+    	if not membre.limiteDepasse(livre):
+       		membre.rendre(livre)
+    	else:
+    		membre.rendre(livre)
+    		return self.fabrique_litiges.creer_litige(livre,membre)	
 
-class LivreNonEmpruntable(Exception):
-	pass
+    def emprunter(self, livre, membre):
+        if not livre.est_empruntable():
+            raise LivreNonEmpruntableError()
+        if not membre.peut_emprunter():
+            raise QuotaAtteintError()
+        # emprunt = Emprunt(livre, membre)
+        # JAMAIS ! SURTOUT PAS !
+        return self.fabrique_emprunts.creer_emprunt(livre, membre)
+
+class LivreNonEmpruntableError(Exception):
+    pass
 
 class QuotaAtteintError(Exception):
-	pass
+    pass
 
-class Livre :
-	"""
-	>>> livre = Livre()
-	>>> livre.est_empruntable()
-	True
-	"""
-	pass
+class Livre:
+    """
+    >>> livre = Livre()
+    >>> livre.est_empruntable()
+    True
+    """
+    pass
 
-class Membre :
-	pass
+class Membre:
+    """
+    >>> membre = Membre()
+    >>> membre.peut_emprunter()
+    True
+    """
+    pass
+
+class FabriqueEmprunts:
+    """
+    Permet de créer des emprunts.
+    >>> fabrique = FabriqueEmprunts()
+    >>> livre = Livre()
+    >>> membre = membre()
+    >>> emprunt = fabrique.creer_emprunt(livre, membre)
+    """
+    pass
+
+class FabriqueLitiges:
+    """
+    Permet de créer des litiges.
+    """
+    pass
--- TP3_CTD/test_bibliotheque.py
+++ TP3_CTD/test_bibliotheque.py
@@ -2,6 +2,15 @@
 PETIT Antoine & WISSOCQ Sarah
 
 Test Bibliotheque
+
+emprunter un livre
+ - si le livre n'est que consultable l'emprunt échoue
+ - si le membre a atteint son quota l'emprunt échoue
+ - sinon un emprunt est créé et disponible pour affichage
+
+rendre un livre
+ - le livre est bien rendu
+ - si le membre a depasse les 30 jours alors signale au service litige
 """
 
 import unittest
@@ -9,25 +18,49 @@ from mockito import *
 from bibliotheque import *
 
 class TestEmprunterUnLivre(unittest.TestCase):
+    def setUp(self):
+        self.livre=mock()
+        self.membre = mock()
+        self.fabrique_emprunts = mock()
+        self.fabrique_litiges = mock()
+        self.service_emprunts = ServiceEmprunt(self.fabrique_emprunts,self.fabrique_litiges)
 
-	def test_livre_consultable_uniquement_l_emprunt_echoue(self):
-		livre = mock()
-		membre = mock()
-		when(livre).est_empruntable().thenReturn(False)
-		service_emprunts = ServiceEmprunt()
-		self.assertRaises(LivreNonEmpruntable,service_emprunts.emprunter,livre,membre)
+    def test_livre_consultable_uniquement_l_emprunt_echoue(self):
+        when(self.livre).est_empruntable().thenReturn(False)
+        self.assertRaises(LivreNonEmpruntableError,self.service_emprunts.emprunter,self.livre,self.membre)
+        
 
-	def test_le_membre_a_atteint_son_quota_l_emprunt_echoue(self):
-		livre = mock()
-		membre = mock()
-		when(membre).peut_emprunter().thenReturn(False)
-		service_emprunts = ServiceEmprunt()
-		self.assertRaises(QuotaAtteintError,service_emprunts.emprunter,livre,membre)
+    def test_le_membre_a_atteint_son_quota_l_emprunt_echoue(self):
+        when(self.livre).est_empruntable().thenReturn(True)
+        when(self.membre).peut_emprunter().thenReturn(False)
+        self.assertRaises(QuotaAtteintError,self.service_emprunts.emprunter,self.livre,self.membre)
+        
+    def test_l_emprunt_est_cree(self):
+        when(self.livre).est_empruntable().thenReturn(True)
+        when(self.membre).peut_emprunter().thenReturn(True)
+        emprunt = mock()
+        when(self.fabrique_emprunts).creer_emprunt(self.livre, self.membre).thenReturn(emprunt)
+        self.assertEqual(self.service_emprunts.emprunter(self.livre,self.membre), emprunt)
+        
+class TestRendreUnLivre(unittest.TestCase):
+    def setUp(self):
+        self.livre=mock()
+        self.membre = mock()
+        self.fabrique_emprunts = mock()
+        self.fabrique_litiges = mock()
+        self.service_emprunts = ServiceEmprunt(self.fabrique_emprunts,self.fabrique_litiges)
 
-	def test_l_emprunt_est_cree(self):
-		livre = mock()
-		membre = mock()
-		when(livre).est_empruntable().thenReturn(True)
-		when(membre).peut_emprunter.thenReturn(True)
-		service_emprunts = ServiceEmprunt()
-		emprunt = service_emprunts.emprunter(livre,membre)
+    def test_livre_bien_rendu(self):
+        self.service_emprunts.rendre(self.livre,self.membre)
+        verify(self.membre).rendre(self.livre)
+
+    def test_livre_plus_30_jours_creer_litige(self):
+        when(self.membre).limiteDepasse(self.livre).thenReturn(True)
+        litige=mock()
+        when(self.fabrique_litiges).creer_litige(self.livre,self.membre).thenReturn(litige)
+        self.assertEqual(self.service_emprunts.rendre(self.livre,self.membre), litige)
+
+    def test_livre_plus_30_jours_livre_bien_rendu(self):
+        when(self.membre).limiteDepasse(self.livre).thenReturn(True)
+        self.service_emprunts.rendre(self.livre,self.membre)
+        verify(self.membre).rendre(self.livre)
